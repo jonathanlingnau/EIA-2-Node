a7111d717f86341c9825fd4a6366f49c46c2f4e9
Asus <Asus@DESKTOP-U65PGTJ.hfu-open.fu.hs-furtwangen.de>
2017-06-21 19:55:05 +0200

    IceParadise

--- NodeTest.js
+++ NodeTest.js
@@ -22,9 +22,15 @@ function handleRequest(_request, _response) {
         console.log(key + ":" + query[key]);
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    for (key in query) {
-        _response.write(key + ":" + query[key]);
-    }
+    //    for (key in query) {
+    //        _response.write(key + ":" + query[key]);
+    //    }
+    _response.write("Vielen Dank f�r Ihre Bestellung!\n\n" + "Bitte pr�fen Sie nocheinmal Ihre Bestellung. Falls diese Fehlerhaft sein sollte wenden Sie sich bitte an unseren Support.\n");
+    _response.write("Schokolade: " + query["Schokolade"] + "\n");
+    _response.write("Erdbeere: " + query["Erdbeere"] + "\n");
+    _response.write("Haselnuss: " + query["Haselnuss"] + "\n");
+    _response.write("Vanille: " + query["Vanille"] + "\n");
+    _response.write("Banane: " + query["Banane"] + "\n");
     _response.end();
 }
 //# sourceMappingURL=NodeTest.js.map
